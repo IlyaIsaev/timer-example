c086525063903eea81180b383f580eb5f2ec77e3
Ilya Isaev <ecmascript8@gmail.com>
2024-02-06 15:28:20 +0600

    fix timer lag

--- src/features/start-pause-timer/model/start-pause-timer.ts
+++ src/features/start-pause-timer/model/start-pause-timer.ts
@@ -1,11 +1,20 @@
 import { TimerModel } from "@entities/timer";
 
-const startTimer = (timer: TimerModel) => {
+const startTimer = (
+  timer: TimerModel,
+  desirableDelay = 100,
+  delayInUse = desirableDelay,
+  startTime = Date.now()
+) => {
   const timeoutId = setTimeout(() => {
     timer.tick();
 
-    startTimer(timer);
-  }, 100);
+    const operationTime = Date.now() - startTime;
+
+    const reducedDelay = desirableDelay - (operationTime - desirableDelay);
+
+    startTimer(timer, desirableDelay, reducedDelay);
+  }, delayInUse);
 
   timer.setTimoutId(timeoutId);
 };
